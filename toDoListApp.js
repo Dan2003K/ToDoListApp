66ac7e9925c34f80e1dc7778a57585fb901ac8a8
Dan <dansharabi2003@gmail.com>
2025-08-30 05:52:49 +0300

    Added task completion counter and Enter keydown Listener

--- toDoListApp.js
+++ toDoListApp.js
@@ -1,37 +1,74 @@
+let taskListContainer = document.querySelector('.container');
 let addTaskBtn = document.querySelector('#addTaskBtn');
 
 // Add Task Button runs addtaskItem function on click
 addTaskBtn.addEventListener('click', addTaskItem);
 
+// Pressing Enter in the input runs addTaskItem()
+taskInput.addEventListener('keydown', e => e.key === "Enter" ? addTaskItem() : null);
+
 /// Add Task Item to the List
 function addTaskItem() {
-    let taskInput = document.querySelector('#taskInput').value;
+    let taskInput = document.querySelector('#taskInput');
     let taskList = document.querySelector('#taskList');
     let newTaskItem = document.createElement('li');
     newTaskItem.classList = "taskItem";
-    if (taskInput == '') {
+    if (taskInput.value == '') {
         document.querySelector('#taskInput').placeholder = "Please enter a task !!!";
         document.querySelector('#taskInput').style.color = "red";
         return;
     } else {
         newTaskItem.innerHTML =
-            `<input type="checkbox">
+            `
+            <section>
+            <input type="checkbox">
             <span>
-            ${taskInput}
+            ${taskInput.value}
             </span>
+            </section>
             <section>
             <button class='editBtn'>Edit</button>
             <button class='delBtn'>Del</button>
-                        </section>
+            </section>
         `;
         taskList.appendChild(newTaskItem);
+        taskInput.value = '';
+        console.log('Task Added');
+        document.querySelector('#taskInput').placeholder = "Add a new task...";
     }
 }
 
-/// Delete Task
+let taskCompleted = document.querySelector('#CompletedTaskCounter');
+
+let counter = 0;
+taskCompleted.innerHTML = `Tasks Completed: ${counter}`;
+
 taskList.addEventListener('click', function (e) {
+    // Delete task
     if (e.target.classList.contains('delBtn')) {
-        e.target.closest('li').remove();
+        const li = e.target.closest('li');
+        const checkbox = li.querySelector('input[type="checkbox"]');
+
+        // If the task being deleted was completed, decrement the counter
+        if (checkbox && checkbox.checked) {
+            counter--;
+        }
+
+        li.remove();
         console.log('Task Deleted');
+        taskCompleted.innerHTML = `Tasks Completed: ${counter}`;
+        return; // stop further processing
+    }
+
+    // Completed tasks
+    if (e.target.type === 'checkbox') {
+        if (e.target.checked) {
+            counter++;
+            console.log('Task Completed');
+        } else {
+            counter--;
+            console.log('Task Uncompleted');
+        }
+        taskCompleted.innerHTML = `Tasks Completed: ${counter}`;
     }
 });
